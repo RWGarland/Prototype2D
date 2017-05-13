dbc963550b4273ba100af5393447d4ed0b223784
RWGarland <rupertwilliamgarland@gmail.com>
2017-05-13 14:34:11 +0100

    Add files via upload

---
+++ Gravity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This allows the food to be dropped into the pot when it hits the trigger that is on top of the copper pot.
+ * In the refernce page*/
+
+public class Gravity : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Sprite")
+        {
+            GameObject.FindWithTag("Sprite").GetComponent<Rigidbody>().isKinematic = false; //This will get triggered when the sprite enters the pot, the sprite will then fall
+        }
+
+        if (other.tag == "Sprite2")
+        {
+            GameObject.FindWithTag("Sprite2").GetComponent<Rigidbody>().isKinematic = false; //This will get triggered when the sprite enters the pot, the sprite will then fall
+        }
+
+    }
+    // all I mainly had to do was add the tags to the objects in unity its self and then change the names in the code.
+}
