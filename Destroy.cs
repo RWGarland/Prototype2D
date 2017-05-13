47759328544139a572960eb3b804b409f8dd8d47
RWGarland <rupertwilliamgarland@gmail.com>
2017-05-13 14:33:36 +0100

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ this allows the food to be destroyed in the pot so it wont fall out the bottem of the pot
+     */
+
+public class Destroy : MonoBehaviour {
+
+    public GameObject Sprite;
+
+	void OnCollisionEnter(Collision Slot) //just a collider that is not set to trigger
+    {
+            Destroy(Sprite); //on collition this will destroy the sprite when it enters the pot
+    }
+    //I had done reserch for another game and then used that to type this piece of code out.
+}
